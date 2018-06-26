5013cb7e4185b3468c3ef73431a492531322193f
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2018-06-26 09:52:20 +0300

    add batch mode to images

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ docker build --build-arg JAR_FILE=./target/flink.jar -t sw-perf-flink:1.0 .
 docker build --build-arg JAR_FILE=./target/flink-s.jar -t sw-perf-flink-s:1.0 .
 docker build --build-arg JAR_FILE=./target/flink-w.jar -t sw-perf-flink-w:1.0 .
 docker build --build-arg JAR_FILE=./target/flink-welt.jar -t sw-perf-flink-welt:1.0 .
+docker build --build-arg JAR_FILE=./target/flink-bw.jar -t sw-perf-flink-bw:1.0 .
+
