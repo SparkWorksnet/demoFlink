d8ab8b59c3e7fc948910ea6420baea18e98e131b
Nikos Kanakis <full290@gmail.com>
2018-12-10 01:32:52 +0200

    sort metrics

--- src/main/java/net/sparkworks/reporter/FileReporter.java
+++ src/main/java/net/sparkworks/reporter/FileReporter.java
@@ -1,6 +1,7 @@
 package net.sparkworks.reporter;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.flink.metrics.Gauge;
 import org.apache.flink.metrics.MetricConfig;
 import org.apache.flink.metrics.reporter.AbstractReporter;
 import org.apache.flink.metrics.reporter.Scheduled;
@@ -8,8 +9,11 @@ import org.apache.flink.metrics.reporter.Scheduled;
 import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 public class FileReporter extends AbstractReporter implements Scheduled {
     
@@ -36,7 +40,9 @@ public class FileReporter extends AbstractReporter implements Scheduled {
         final StringBuilder header = new StringBuilder(String.valueOf("#" + timestamp));
         final StringBuilder values = new StringBuilder(String.valueOf(timestamp));
         AtomicBoolean hasMetrics = new AtomicBoolean(false);
-        this.gauges.forEach((gauge, gaugeName) -> {
+        this.gauges
+                .entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+                .forEach((gauge, gaugeName) -> {
             if ((gaugeName.contains("Status_JVM_CPU") ||
                     gaugeName.contains("Heap_Used") ||
                     gaugeName.contains("NonHeap_Used") ||
@@ -47,7 +53,9 @@ public class FileReporter extends AbstractReporter implements Scheduled {
                 hasMetrics.set(true);
             }
         });
-        this.meters.forEach((meter, meterName) -> {
+        this.meters
+                .entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+                .forEach((meter, meterName) -> {
             if ((meterName.contains("numRecords") || meterName.contains("System_CPU")  || meterName.contains("System_Memory") || meterName.contains("System_Swap")) &&
                     !meterName.contains("jobmanager")) {
                 header.append("@" + meterName);
@@ -55,7 +63,9 @@ public class FileReporter extends AbstractReporter implements Scheduled {
                 hasMetrics.set(true);
             }
         });
-        this.counters.forEach((counter, counterName) -> {
+        this.counters
+                .entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new))
+                .forEach((counter, counterName) -> {
             if ((counterName.contains("numRecords") || counterName.contains("System_CPU") || counterName.contains("System_Memory") || counterName.contains("System_Swap")) &&
                     !counterName.contains("jobmanager")) {
                 header.append("@" + counterName);
