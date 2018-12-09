e2141196512a825f15c1a912357ade5bda3ad4dd
Nikos Kanakis <full290@gmail.com>
2018-12-10 01:13:55 +0200

    csv reporter fix

--- src/main/java/net/sparkworks/reporter/FileReporter.java
+++ src/main/java/net/sparkworks/reporter/FileReporter.java
@@ -7,9 +7,9 @@ import org.apache.flink.metrics.reporter.Scheduled;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FileReporter extends AbstractReporter implements Scheduled {
     
@@ -35,31 +35,35 @@ public class FileReporter extends AbstractReporter implements Scheduled {
         final File file = new File("/tmp/results.csv");
         final StringBuilder header = new StringBuilder(String.valueOf("#" + timestamp));
         final StringBuilder values = new StringBuilder(String.valueOf(timestamp));
+        AtomicBoolean hasMetrics = new AtomicBoolean(false);
         this.gauges.forEach((gauge, gaugeName) -> {
             if ((gaugeName.contains("Status.JVM.CPU") ||
                     gaugeName.contains("Heap.Used") ||
                     gaugeName.contains("NonHeap.Used") ||
                     gaugeName.contains("System.CPU") || gaugeName.contains("System.Memory") || gaugeName.contains("System.Swap")) &&
                     !gaugeName.contains("jobmanager")) {
-                header.append("@" + gaugeName.replace(".", "_"));
+                header.append("@" + gaugeName);
                 values.append("@" + gauge.getValue());
+                hasMetrics.set(true);
             }
         });
         this.meters.forEach((meter, meterName) -> {
             if ((meterName.contains("numRecords") || meterName.contains("System.CPU")  || meterName.contains("System.Memory") || meterName.contains("System.Swap")) &&
                     !meterName.contains("jobmanager")) {
-                header.append("@" + meterName.replace(".", "_"));
+                header.append("@" + meterName);
                 values.append("@" + meter.getRate());
+                hasMetrics.set(true);
             }
         });
         this.counters.forEach((counter, counterName) -> {
             if ((counterName.contains("numRecords") || counterName.contains("System.CPU") || counterName.contains("System.Memory") || counterName.contains("System.Swap")) &&
                     !counterName.contains("jobmanager")) {
-                header.append("@" + counterName.replace(".", "_"));
+                header.append("@" + counterName);
                 values.append("@" + counter.getCount());
+                hasMetrics.set(true);
             }
         });
-        if (Objects.nonNull(file)) {
+        if (Objects.nonNull(file) && hasMetrics.get()) {
             try {
                 FileUtils.writeStringToFile(file, header.toString() + lineSeparator + values.toString() + lineSeparator, true);
             } catch (IOException e) {
